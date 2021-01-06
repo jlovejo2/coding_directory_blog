dc8c0d9b046a53a6e2a20212e4641619032c2fb3
James Lovejoy <james.lovejoy2@gmail.com>
2021-01-05 19:02:39 -0600

    initial push

---
+++ pages/index.tsx
@@ -0,0 +1,62 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+import Header from "../components/header";
+import { GetStaticProps } from "next";
+import { Fragment } from "react";
+
+interface Props {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}
+
+export default function Home({ allPostsData }: Props) {
+  return (
+    <Fragment>
+    <Header />
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this in{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className="flex">
+          {allPostsData.map(({ id, date, title }) => (
+            <li className="mr-6" key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a className="text-blue-500 hover:text-blue-800">{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+    </Fragment>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
